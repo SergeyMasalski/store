3668125da1a08e60264a133427ae0a3ceb3572b0
unknown <kadet_2003@list.ru>
2024-03-23 19:29:04 +0700

    added change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 import Layout from './components/Layout/component';
 import styles from './style.module.scss';
+import Registration from "./components/Registration/component.tsx";
+
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
-import store from './features/store.ts'
+import { store } from './features/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
